9e6f17748fb38e2f3ba1d54d42bae2529614e65d
gschare <grschare1315@gmail.com>
2021-03-28 03:17:18 -0700

    Tidying up

--- template.js
+++ template.js
@@ -1,7 +1,7 @@
 var header = `
     <header>
         <nav style="text-align: center">
-            <a href="/home.html">Home</a> &#x2022;
+            <a href="/index.html">Home</a> &#x2022;
             <a href="/about.html">About</a> &#x2022;
             <a href="/teaching.html">Teaching</a> &#x2022;
             <a href="https://github.com/gschare">GitHub</a>
@@ -11,8 +11,22 @@ var header = `
 var footer = `
     <footer>
         <hr>
-        <p>Why are you <em>here</em>?</p>
+        <p>We're off you know...</p>
+        <button onclick="toggleDarkMode()" type="button" id="dark-mode-button"></button>
     </footer>`
 
-document.getElementById("header").innerHTML = header;
-document.getElementById("footer").innerHTML = footer;
+document.getElementById('header').innerHTML = header;
+document.getElementById('footer').innerHTML = footer;
+
+function toggleDarkMode() {
+    var containerClasses = document.getElementById('container').classList;
+    if (containerClasses.contains('dark-mode')) {
+        containerClasses.remove('dark-mode');
+        document.getElementById('dark-mode-button').innerHTML = "Dark mode";
+    } else {
+        containerClasses.add('dark-mode');
+        document.getElementById('dark-mode-button').innerHTML = "Light mode";
+    }
+}
+
+toggleDarkMode();
