89ce746aa94b8bc82b5e9677f6912c2fe0b5d9a4
Gregory Schare <grschare1315@gmail.com>
2023-07-10 14:41:40 -0400

    add blog

---
+++ blog.js
@@ -0,0 +1,6 @@
+// Dynamically fills `blog.html` with links and previews of blog posts from
+// `blog/` folder.
+
+//if (document.getElementById('posts')) {
+//    document.getElementById('posts').innerHTML = 'hi';
+//}
--- template.js
+++ template.js
@@ -1,49 +1,52 @@
 // The dot that goes in between header elements is &#x2022;
-var header = `
+const header = `
     <header>
-        <nav style="text-align: center">
+        <nav>
+            <a href="./index.html">Home</a>
+            &centerdot;
+            <a href="./blog.html">Blog</a>
         </nav>
     </header>`
 
-var toggleDarkButton = `
+const toggleDarkButton = `
         <button onclick="toggleDarkMode()" type="button" id="dark-mode-button"></button>
         `
 
 
-var footer = `
-    <footer>` + toggleDarkButton + `
-        <button onclick="toggleMainDiv()" type="button" id="hide-button">&#x25B2;</button>
-    </footer>`
+//var footer = `
+//    <footer>` + toggleDarkButton + `
+//        <button onclick="toggleMainDiv()" type="button" id="hide-button">&#x25B2;</button>
+//    </footer>`
 
 if (document.getElementById('header')) {
     document.getElementById('header').innerHTML = header;
 }
 
-if (document.getElementById('footer')) {
-    document.getElementById('footer').innerHTML = footer;
-}
+//if (document.getElementById('footer')) {
+//    document.getElementById('footer').innerHTML = footer;
+//}
 
 if (document.getElementById('toggleDarkButton')) {
     document.getElementById('toggleDarkButton').innerHTML = toggleDarkButton;
 }
 
-function toggleMainDiv() {
-    var main = document.getElementById('main');
-    var hide_button = document.getElementById('hide-button');
-    if (main.style.display === "none") {
-        if (document.getElementById('header')) {
-            document.getElementById('header').style.display = "block";
-        }
-        main.style.display = "block";
-        hide_button.innerHTML = "&#x25B2;"
-    } else {
-        if (document.getElementById('header')) {
-            document.getElementById('header').style.display = "none";
-        }
-        main.style.display = "none";
-        hide_button.innerHTML = "&#x25BC;"
-    }
-}
+//function toggleMainDiv() {
+//    var main = document.getElementById('main');
+//    var hide_button = document.getElementById('hide-button');
+//    if (main.style.display === "none") {
+//        if (document.getElementById('header')) {
+//            document.getElementById('header').style.display = "block";
+//        }
+//        main.style.display = "block";
+//        hide_button.innerHTML = "&#x25B2;"
+//    } else {
+//        if (document.getElementById('header')) {
+//            document.getElementById('header').style.display = "none";
+//        }
+//        main.style.display = "none";
+//        hide_button.innerHTML = "&#x25BC;"
+//    }
+//}
 
 function toggleDarkMode() {
     var containerClasses = document.body.classList;
